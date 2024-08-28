451e9f337c3565da4314434f0e2bb3e5073073de
Kolade Amire <stephamire@gmail.com>
2024-08-28 09:39:19 -0500

    project dependencies initialization

---
+++ src/main/java/com/kolade/demo_spring_oauth2/DemoSpringOauth2Application.java
@@ -0,0 +1,13 @@
+package com.kolade.demo_spring_oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kolade/demo_spring_oauth2/DemoSpringOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kolade.demo_spring_oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
